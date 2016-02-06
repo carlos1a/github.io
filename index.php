856b76e92b376437a50b998fd45976b6e4e70d21
carlos1a <cg_47@hotmai.com>
2016-02-05 22:11:12 -0430

    modificado: css/agency.css 	modificado: index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <html lang="en">
 <head>
 
+
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/main.js"></script>
+
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
