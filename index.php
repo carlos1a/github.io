1b75cac744fbec2d67ba799f587d7f12802419b7
carlos1a <cg_47@hotmai.com>
2016-02-06 20:54:02 -0430

    insta

--- index.php
+++ index.php
@@ -1,42 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="js/main.js"></script>
-
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Regaze, desarrollo, web, landing, page, startup, design, developer</title>
-
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom CSS -->
     <link href="css/agency.css" rel="stylesheet">
-
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-<link href='logo/logo.css' rel='stylesheet' type='text/css'>
+    <link href='logo/logo.css' rel='stylesheet' type='text/css'>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
-
 <body id="page-top" class="index">
 
     <!-- Navigation -->
@@ -343,8 +333,7 @@
                             </li>
                             <li><a href="#"><i class="fa fa-github"></i></a>
                             </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
+                            <li><a href="https://instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a>
                         </ul>
                     </div>
                 </div>
@@ -356,10 +345,10 @@
                         <ul class="list-inline social-buttons">
                             <li><a href="#"><i class="fa fa-twitter"></i></a>
                             </li>
-                            <li><a href="#"><i class="fa fa-github"></i></a>
+                            <li><a href="https://github.com/carlos1a" target="_blank"><i class="fa fa-github"></i></a>
                             </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
+                            <li><a href="https://instagram.com/carlos1a/" target="_blank"><i class="fa fa-instagram"></i></a>
+                        </li>
                         </ul>
                     </div>
                 </div>
@@ -373,8 +362,7 @@
                             </li>
                             <li><a href="#"><i class="fa fa-github"></i></a>
                             </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
+                            <li><a href="https://instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a>
                         </ul>
                     </div>
                 </div>
